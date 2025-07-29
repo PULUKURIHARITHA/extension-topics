d79cb1f881c0703f25acfdb394f36db0ca49bfa4
PULUKURIHARITHA <divyalakshmanrao1@gmail.com>
2025-07-29 16:07:07 +0530

    Update task.py

--- task.py
+++ task.py
@@ -7,3 +7,10 @@ with open(filename, 'r') as file:
         if char==letter:
             count +=1
 print(letter,"appears",count,"time in the file") 
+
+
+
+O/P:-
+enter filename with extension :char.txt
+enter a char to search frequency:s
+s appears 4 time in the file
